56def97e310b69d2c57d821b9789bf3ce0c3dad0
DJONDANG AOUDOU <82848851+Djondang@users.noreply.github.com>
2023-11-29 17:06:51 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -7,7 +7,34 @@ def main():
     """
     Main
     """
-    pass
+    data = import_data()
+    data = rename_columns(data)
+
+    
+def import_data() -> pd.DataFrame:
+    """
+    Import csv file as a dataframe
+    Output: data [pd.DataFrame]
+    """
+    data = pd.read_csv("data/iris.csv")
+    print(data.shape)
+    return data
+
+def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Doc
+    """
+    data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
+                                "sepal.width": 'sepal_width',
+                                "petal.length": 'petal_length',
+                                "petal.width": 'petal_width'})
+    
+    return data_renamed
+def random_row() -> pd.DataFrame:
+    data = pd.read_csv("data/iris.csv")
+    random_lines = random.sample(data, 50)
+    for line in random_lines:
+        print(data.strip())
 
 
 if __name__ == '__main__':
