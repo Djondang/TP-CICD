443881372080e3377d8365170372d82f8f74f2f1
Djondang <joulebecobusi@gmail.com>
2023-12-05 09:56:27 +0100

    Final commit

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 """
 CI/CD
 """
+import os
+print("Répertoire de travail actuel :", os.getcwd())
+
 import pandas as pd
 import random
 
@@ -10,12 +13,16 @@ def main():
     """
     data = import_data()
     data = rename_columns(data)
+    data = random_row(data, 50)
+    data = concat_dataset(data)
+    data = column_totalsepal(data)
+    data = column_totalpetal(data)
 
     
 def import_data() -> pd.DataFrame:
     """
-    Import csv file as a dataframe
-    Output: data [pd.DataFrame]
+    Fonction pour importer un fichier CSV en tant que DataFrame.
+    Sortie : data [pd.DataFrame]
     """
     data = pd.read_csv("data/iris.csv")
     print(data.shape)
@@ -23,19 +30,50 @@ def import_data() -> pd.DataFrame:
 
 def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Doc
+    Fonction pour renommer certaines colonnes du DataFrame.
     """
     data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
                                 "sepal.width": 'sepal_width',
                                 "petal.length": 'petal_length',
                                 "petal.width": 'petal_width'})
-    
     return data_renamed
-def random_row() -> pd.DataFrame:
-    data = pd.read_csv("data/iris.csv")
-    random_lines = random.sample(data, 50)
-    for line in random_lines:
-        print(data.strip())
+
+def random_row(data: pd.DataFrame, num_rows: int) -> pd.DataFrame:
+    """
+    Fonction pour sélectionner un nombre spécifié de lignes au hasard à partir du DataFrame.
+    Entrées :
+    - data : DataFrame
+    - num_rows : Nombre de lignes à sélectionner au hasard
+    Sortie :
+    - DataFrame contenant les lignes sélectionnées
+    """
+    random_rows = data.sample(n=num_rows, random_state=42)  
+    return random_rows
+
+def concat_dataset(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fonction pour concaténer les données sélectionnées plusieurs fois.
+    """
+    data_rows = random_row(data, 50)
+    concat_data = pd.concat([data_rows, data_rows, data_rows], ignore_index=True)
+    return concat_data
+
+def column_totalsepal(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fonction pour ajouter une nouvelle colonne contenant la somme de 'sepal_length' et 'sepal_width'.
+    """
+    data["total_sepal"] = data.sepal_length + data.sepal_width
+    print(data.head())
+    return data
+
+def column_totalpetal(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fonction pour ajouter une nouvelle colonne contenant la somme de 'petal_length' et 'petal_width'.
+    """
+    data["total_petal"] = data.petal_length + data.petal_width
+    print(data.head())
+    return data
+
 
 
 if __name__ == '__main__':
--- test_run.py
+++ test_run.py
@@ -1,20 +1,91 @@
 """
 Tests
 """
-from run import import_data, rename_columns
+from run import import_data, rename_columns, random_row, concat_dataset, column_totalsepal, column_totalpetal
 
 def test_import_data():
     """
-    Test
+    Test la fonction import_data
     """
     data = import_data()
     assert data.shape[0] > 0
 
-
 def test_rename_columns():
     """
-    Doc
+    Test la fonction rename_columns
     """
     data = import_data()
     data_renamed = rename_columns(data)
     assert "sepal_length" in data_renamed.columns
+
+def test_random_row():
+    """
+    Test la fonction random_row
+    """
+    data = import_data()
+    random_lines = random_row(data, 50)
+    assert len(random_lines) == 50
+
+def test_concat_dataset():
+    """
+    Test la fonction concat_dataset
+    """
+    data = import_data()
+    concatenated_data = concat_dataset(data)
+    assert concatenated_data.shape[0] == 3 * 50  # Trois fois le nombre initial de lignes
+
+def test_column_totalsepal():
+    """
+    Test la fonction column_totalsepal
+    """
+    data = import_data()
+    data_renamed = rename_columns(data)
+    data_with_totals = column_totalsepal(data_renamed)
+    assert "total_sepal" in data_with_totals.columns
+
+def test_column_totalpetal():
+    """
+    Test la fonction column_totalpetal
+    """
+    data = import_data()
+    data_renamed = rename_columns(data)
+    data_with_totals = column_totalpetal(data_renamed)
+    assert "total_petal" in data_with_totals.columns
+
+
+
+# """
+# Tests
+# """
+# from run import import_data, rename_columns,random_rows,concat_dataset,column_totalsepal,column_totalpetal
+
+
+# def test_import_data():
+#     """
+#     Test
+#     """
+#     data = import_data()
+#     assert data.shape[0] > 0
+
+
+# def test_rename_columns():
+#     """
+#     Doc
+#     """
+#     data = import_data()
+#     data_renamed = rename_columns(data)
+#     assert "sepal_length" in data_renamed.columns
+
+# def test_random_rows():
+#     """"
+#     Doc
+#     """
+#     data = import_data()
+#     few_line = random_rows(data,50)
+#     assert len(few_line) == 50
+
+# # def test_concact_dataset():
+# #     """
+# #     Doc
+# #     """
+
